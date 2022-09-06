8c431d77f9e3c080106c443333abeeb541e2fe9d
Teemu Koivisto <tkoivisto456@gmail.com>
2022-09-06 22:12:28 +0300

    init repo with svelte-kit & tailwind

---
+++ packages/client/src/global.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_API_URL?: string
+  VITE_WS_URL?: string
+}
---
+++ packages/client/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<section class="p-4 h-full m-auto lg:container md:p-16 md:pt-8 xs:p-8 rounded-2xl">
+  <header class="flex flex-col items-center mt-8">
+    <h1 class="my-3 text-5xl font-bold flex items-center">TeemuKoivisto</h1>
+    <em>Hello!</em>
+  </header>
+</section>
+
+<style lang="scss">
+</style>
